d84c21c300fd7ce0296bcb7a376f66532ae53015
BlueRaja <BlueRaja.admin@gmail.com>
2014-10-12 13:24:04 -0500

    Added classes to represent all known AppId's/ContextId's for inventories

---
+++ SteamTrade/Inventory/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamTrade.Inventory
+{
+    /// <summary>
+    /// Represents the "AppId", or game to which the inventory belongs.
+    /// Note that a game may have more than one type of Inventory, eg. Steam has Gifts, Coupons, and Community
+    /// </summary>
+    public enum Game
+    {
+        BattleBlockTheater = 238460,
+        CSGO = 730,
+        Dota2 = 570,
+        PathOfExile = 238960,
+        Portal2 = 620,
+        SinsOfADarkAge = 251970,
+        SpiralKnights = 99900,
+        Steam = 753,
+        SuperMondayNightCombat = 104700,
+        TeamFortress2 = 440,
+        Warframe = 230410,
+    }
+}
---
+++ SteamTrade/Inventory/InventoryType.cs
@@ -0,0 +1,65 @@
+﻿namespace SteamTrade.Inventory
+{
+    /// <summary>
+    /// A single game may have multiple "types" of inventories.  This immutable class represents a specific inventory type for a specific game.
+    /// </summary>
+    public class InventoryType
+    {
+        public static readonly InventoryType BattleBlockTheater = new InventoryType(Game.BattleBlockTheater, 2);
+        public static readonly InventoryType CSGO = new InventoryType(Game.CSGO, 2);
+        public static readonly InventoryType Dota2 = new InventoryType(Game.Dota2, 2);
+        public static readonly InventoryType PathOfExile = new InventoryType(Game.PathOfExile, 1);
+        public static readonly InventoryType Portal2 = new InventoryType(Game.Portal2, 2);
+        public static readonly InventoryType SinsOfADarkAge = new InventoryType(Game.SinsOfADarkAge, 1);
+        /// <summary>
+        /// Spiral Knights inventory.
+        /// Because the "types" of inventories are custom to each user, the ContextId for this InventoryType.SpiralKnights is invalid.
+        /// Additionally, it means loading a SpiralKnights potentially requires many more web requests than for other games.
+        /// </summary>
+        public static readonly InventoryType SpiralKnights = new InventoryType(Game.SpiralKnights, 0);
+        /// <summary>
+        /// Contains all tradable Steam games
+        /// </summary>
+        public static readonly InventoryType Steam_Gifts = new InventoryType(Game.Steam, 1);
+        /// <summary>
+        /// Contains all tradable Steam games
+        /// </summary>
+        public static readonly InventoryType Steam_Coupons = new InventoryType(Game.Steam, 3);
+        /// <summary>
+        /// Contains all cards, backgrounds, and emotes
+        /// </summary>
+        public static readonly InventoryType Steam_Community = new InventoryType(Game.Steam, 6);
+        /// <summary>
+        /// Contains "item rewards" for random non-inventory games, usually given out during Steam holiday events
+        /// </summary>
+        public static readonly InventoryType Steam_ItemRewards = new InventoryType(Game.Steam, 7);
+        public static readonly InventoryType SuperMondayNightCombat_Products = new InventoryType(Game.SuperMondayNightCombat, 1);
+        public static readonly InventoryType SuperMondayNightCombat_Uniforms = new InventoryType(Game.SuperMondayNightCombat, 2);
+        public static readonly InventoryType SuperMondayNightCombat_Endorsements = new InventoryType(Game.SuperMondayNightCombat, 3);
+        public static readonly InventoryType SuperMondayNightCombat_Pros = new InventoryType(Game.SuperMondayNightCombat, 5);
+        public static readonly InventoryType SuperMondayNightCombat_Weapons = new InventoryType(Game.SuperMondayNightCombat, 7);
+        public static readonly InventoryType SuperMondayNightCombat_Taunts = new InventoryType(Game.SuperMondayNightCombat, 10);
+        public static readonly InventoryType SuperMondayNightCombat_Misc = new InventoryType(Game.SuperMondayNightCombat, 12);
+        public static readonly InventoryType SuperMondayNightCombat_Flair = new InventoryType(Game.SuperMondayNightCombat, 13);
+        public static readonly InventoryType SuperMondayNightCombat_TreasureBalls = new InventoryType(Game.SuperMondayNightCombat, 14);
+        public static readonly InventoryType TeamFortress2 = new InventoryType(Game.TeamFortress2, 2);
+        public static readonly InventoryType Warframe = new InventoryType(Game.Warframe, 2);
+
+        /// <summary>
+        /// A number representing the "subtype" of the inventory.  For instance, Steam coupons, Steam backgrounds/emotes/cards,
+        /// and Steam gifts all have different context id's.
+        /// </summary>
+        public readonly int ContextId;
+
+        /// <summary>
+        /// The game which this type of inventory belongs to
+        /// </summary>
+        public readonly Game Game;
+
+        private InventoryType(Game game, int contextId)
+        {
+            Game = game;
+            ContextId = contextId;
+        }
+    }
+}
