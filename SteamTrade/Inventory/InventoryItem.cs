ba9e86d5096f2b2b57251f000fe20f105ef67ef1
BlueRaja <BlueRaja.admin@gmail.com>
2014-10-18 18:10:31 -0500

    Wrote the beginnings of the code to download and parse the Inventory

---
+++ SteamTrade/Inventory/InventoryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamTrade.Inventory
+{
+    public class InventoryItem
+    {
+        public InventoryType InventoryType { get; internal set; }
+        public string DisplayName { get; internal set; }
+        public string OriginalName { get; internal set; }
+        public bool IsTradable { get; internal set; }
+        public bool IsMarketable { get; internal set; }
+        public string IconUrl { get; internal set; }
+        public string Type { get; internal set; }
+        public long Id { get; internal set; }
+        public int InventoryPosition { get; internal set; }
+        public string NameColor { get; internal set; }
+        public string BackgroundColor { get; internal set; }
+    }
+}
---
+++ SteamTrade/Inventory/InventoryJsonDownloader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using SteamKit2;
+
+namespace SteamTrade.Inventory
+{
+    internal class InventoryJsonDownloader
+    {
+        private readonly SteamWeb _steamWeb;
+
+        public InventoryJsonDownloader(SteamWeb steamWeb)
+        {
+            _steamWeb = steamWeb;
+        }
+
+        /// <summary>
+        /// Returns the inventory/json response for the given user/inventoryType.
+        /// Works even when the user has a private backback.  However, only works if user sent us a trade offer.
+        /// </summary>
+        public string GetTradeOfferInventoryJson(SteamID owner, InventoryType inventoryType)
+        {
+            string url = @"http://steamcommunity.com/tradeoffer/new/partnerinventory/";
+
+            var data = new NameValueCollection
+            {
+                { "sessionid", _steamWeb.SessionId },
+                { "partner", owner.ConvertToUInt64().ToString() },
+                { "appid", inventoryType.Game.ToString() },
+                { "contextid", inventoryType.ContextId.ToString() }
+            };
+
+            return _steamWeb.Fetch(url, "GET", data);
+        }
+
+        /// <summary>
+        /// Returns the inventory/json response for the given user/inventoryType.
+        /// Works even when the user has a private backback.  However, only works if we are currently trading with that user.
+        /// </summary>
+        public string GetTradeInventoryJson(SteamID owner, InventoryType inventoryType)
+        {
+            string url = String.Format(@"http://steamcommunity.com/trade/{0}/foreigninventory/", owner.ConvertToUInt64());
+
+            var data = new NameValueCollection
+            {
+                { "sessionid", _steamWeb.SessionId },
+                { "steamid", owner.ConvertToUInt64().ToString() },
+                { "appid", inventoryType.Game.ToString() },
+                { "contextid", inventoryType.ContextId.ToString() }
+            };
+
+            return _steamWeb.Fetch(url, "GET", data);
+        }
+
+        /// <summary>
+        /// Returns the inventory/json response for the given user/inventoryType.
+        /// Only works when the user's backpack is non-private.
+        /// </summary>
+        public string GetInventoryJson(SteamID owner, InventoryType inventoryType)
+        {
+            string url = String.Format(@"http://steamcommunity.com/profiles/{0}/inventory/json/{1}/{2}/",
+                owner.ConvertToUInt64(), inventoryType.Game, inventoryType.ContextId);
+
+            return _steamWeb.Fetch(url, "GET");
+        }
+    }
+}
