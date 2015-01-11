a001e4299cd1483109f93208677dbb2a6ae3b02f
WildCard65 <lollol222gg@gmail.com>
2015-01-11 08:46:35 -0500

    Possible fix for userhandler constructors getting NULL_STEAMID

--- SteamBot/UserHandler.cs
+++ SteamBot/UserHandler.cs
@@ -37,6 +37,8 @@ namespace SteamBot
 
         public UserHandler(Bot bot, SteamID sid)
         {
+            if (sid == bot.NULL_STEAMID)
+                return;
             Bot = bot;
             OtherSID = sid;
             GetOtherInventory();
